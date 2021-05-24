18caf40135127ac9cd46e862ba5d5d23d8a6e4aa
tlatimer <tom@tomlat.com>
2021-05-24 14:49:00 -0500

    added get_groups and supporting code to segment the map into contiguous chunks

--- CaveGame.py
+++ CaveGame.py
@@ -1,8 +1,11 @@
 from random import choices
 from copy import copy
+from collections import deque
+
 
 def main():
     b = Board(120, 40)
+
 
 class Board:
     def __init__(self, w, h):
@@ -12,51 +15,94 @@ class Board:
         for x in range(w):
             self.walls.append([True] * h)
 
-        self.generate_cave()
-
-    def print_walls(self):
-        for y in range(self.h):
-            for x in range(self.w):
-                if self.walls[x][y]:
-                    print('#', end='')
-                else:
-                    print(' ', end='')
-            print()
-        print('\n')
-
-    def generate_cave(self):
-        self.all_coords = []
+        self.all_coords = []  # valid internal coords
         for x in range(1, self.w - 1):
             for y in range(1, self.h - 1):
                 self.all_coords.append((x, y))
 
+        self.generate_cave()
+        self.groups = self.get_groups()
+        self.print_board(True)
+        print(len(self.groups[0]) / (self.w * self.h))
+
+    def print_board(self, with_groups=False):
+        for y in range(self.h):
+            for x in range(self.w):
+                if not with_groups:
+                    if self.walls[x][y]:
+                        print('#', end='')
+                    else:
+                        print(' ', end='')
+                else:
+                    if self.walls[x][y]:
+                        print(' ', end='')
+                        continue
+
+                    for i, group in enumerate(self.groups):
+                        if (x, y) in group:
+                            print(chr(65 + i), end='')
+            print()
+        print('\n')
+
+    def generate_cave(self):
         # initialize to random
-        num_to_blank = int(self.h * self.w * 0.45)
-        for coords in choices(self.all_coords, k=num_to_blank):
-            x, y = coords
+        num_to_blank = int(self.h * self.w * 0.55)
+        for x, y in choices(self.all_coords, k=num_to_blank):
             self.walls[x][y] = False
 
         for _ in range(2):
-            self.print_walls()
-            self.do_ca_step()
+            self.do_ca_step()  # ca = cellular automata
 
     def do_ca_step(self):
-        walls_copy = copy(self.walls)
+        walls_old = copy(self.walls)
         for x, y in self.all_coords:
-            if self.count_nonwall_neighbor(x, y, walls_copy) < 3:
+            if len(self.get_neighbors(x, y, walls_old)) < 3:
                 self.walls[x][y] = True
-            elif self.count_nonwall_neighbor(x, y, walls_copy) > 4:
+
+            elif len(self.get_neighbors(x, y, walls_old)) > 4:
                 self.walls[x][y] = False
 
-    def count_nonwall_neighbor(self, x, y, walls_list):
-        num_nonwall = 0
+    def get_neighbors(self, x, y, walls_grid=None):
+        if not walls_grid:
+            walls_grid = self.walls
+
+        neighbors = []
         for xvar in [-1, 0, 1]:
             for yvar in [-1, 0, 1]:
                 if (xvar, yvar) == (0, 0):
                     continue
-                if not walls_list[x + xvar][y + yvar]:
-                    num_nonwall += 1
-        return num_nonwall
+                if not walls_grid[x + xvar][y + yvar]:
+                    neighbors.append((x + xvar, y + yvar))
+
+        return neighbors
+
+    def get_groups(self):
+        groups = []
+        visited = set()
+        to_visit = deque()
+        for x, y in self.all_coords:
+            if self.walls[x][y]:
+                continue
+            elif (x, y) in visited:
+                continue
+            else:
+                # begin BFS from this point
+                to_visit.append((x, y))
+                my_group = []
+                while to_visit:
+                    cur = to_visit.popleft()
+                    my_group.append(cur)
+                    visited.add(cur)
+
+                    cur_x, cur_y = cur
+                    for n in self.get_neighbors(cur_x, cur_y):
+                        if n not in visited and n not in to_visit:
+                            to_visit.append(n)
+
+                groups.append(my_group)
+
+        groups.sort(key=lambda i: len(i), reverse=True)
+        return groups
 
 
 if __name__ == '__main__':
