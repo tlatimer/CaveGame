74f62c57aeb594f033d6a1a32139d30728bc2550
tlatimer <tom@tomlat.com>
2021-05-25 13:43:01 -0500

    working with maximizing the screen.

--- CaveGame.py
+++ CaveGame.py
@@ -1,44 +1,39 @@
 import pygame as pg
-import pygame.draw
 
 import GenerateBoard as gb
 
 
-WIDTH = 1024
-HEIGHT = 768
-
-TILE_SIZE = 8
-
-assert WIDTH % TILE_SIZE == 0 and HEIGHT % TILE_SIZE == 0
-
-
 class CaveGame:
     def __init__(self):
-        pg.init()
-        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
-        pg.display.set_caption('Cave Game')
 
-        background = pg.Surface(self.screen.get_size())
-        background = background.convert()
-        background.fill('blue4')
-        self.screen.blit(background, (0,0))
+        pg.init()
+
+        pg.display.set_caption('Cave Game')
+        self.screen = pg.display.set_mode((0, 0), pg.WINDOWMAXIMIZED)
+
+        bg = pg.Surface(self.screen.get_size())
+        bg = bg.convert()
+        bg.fill('blue4')
+        self.screen.blit(bg, (0,0))
 
         loading = pg.font.Font(None, 36).render("LOADING", 1, 'white')
-        loading_pos = loading.get_rect(centerx=background.get_width() / 2, centery=background.get_height() / 2)
+        loading_pos = loading.get_rect(centerx=bg.get_width() / 2, centery=bg.get_height() / 2)
         self.screen.blit(loading, loading_pos)
         pg.display.flip()
         # leave loading screen up while generating board
 
-        self.board = gb.GenerateBoard(WIDTH / TILE_SIZE, HEIGHT / TILE_SIZE)
+        tile_size = 16 # TODO: make this dynamic
+        gb_args = (self.screen.get_width() / tile_size, self.screen.get_height() / tile_size)
+        self.board = gb.GenerateBoard(*gb_args)
 
-        self.screen.blit(background, (0, 0))
+        self.screen.blit(bg, (0, 0))
         # reblank background before drawing tiles
         for x, y in self.board.get_walls_coords():
             r = pg.Rect(
-                x * TILE_SIZE,
-                y * TILE_SIZE,
-                TILE_SIZE,
-                TILE_SIZE,
+                x * tile_size,
+                y * tile_size,
+                tile_size,
+                tile_size,
             )
             pg.draw.rect(self.screen, 'black', r)
 
@@ -47,14 +42,15 @@ class CaveGame:
         try:
             while True:
                 pg.display.flip()
-                event = pg.event.wait()
 
+                event = pg.event.wait()
                 if event.type == pg.QUIT:
+                    break
+                elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
                     break
                 elif event.type == pg.MOUSEBUTTONDOWN:
                     # TODO: figure out where the click was and do stuff
                     self.__init__()
-                    pass
         finally:
             pg.quit()
 
